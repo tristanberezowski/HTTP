4e0e190dd5a1a8d564d2aa1b751857ac0bc437ae
Tristan <tristanberezowski@gmail.com>
2019-02-11 19:53:38 +0000

    step one done

--- client.js
+++ client.js
@@ -1,10 +1,26 @@
-function getAndPrintHTMLChunks () {
+function getAndPrintHTMLChunks () { //logs each chunk of data as it is received
 
   var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
 
-  /* Add your code here */
+  var https = require('https');   
 
+  https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+  
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+    });
+  
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
 }
